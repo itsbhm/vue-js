f1734c6227e637ab4dd4e27703eb283b5f296428
Shubham <shubham@itsbhm.group>
2021-03-07 11:21:29 +0530

    Added List Rendering

---
+++ assets/js/list-rendering.js
@@ -0,0 +1,15 @@
+let vm = new Vue({
+
+    el: '#app',
+    data: {
+        message: 'Hello',
+        posts: [
+            {id: 1, title: 'Post 1', body: 'This is post 1'},
+            {id: 2, title: 'Post 2', body: 'This is post 2'},
+            {id: 3, title: 'Post 3', body: 'This is post 3'},
+            {id: 4, title: 'Post 4', body: 'This is post 4'},
+            {id: 5, title: 'Post 5', body: 'This is post 5'}
+        ]
+    }
+
+})
